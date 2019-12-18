d660d4da120febc72d9fa8ed617e253def31275c
Dennis Basyrov <dennis.basyrov@gmail.com>
2019-12-18 14:53:44 -0500

    ! pattern

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,15 +16,26 @@ class UsersController < ApplicationController
         render json: @user
     end
 
+    # def create
+    #     @user = User.create(user_params)
+    #     # byebug
+    #     if @user.valid?
+    #         @token = encode_token(user_id: @user.id)
+    #         render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    #     else
+    #         render json: { error: 'failed to create user' }, status: :not_acceptable
+    #     end
+    # end
+
     def create
         @user = User.create(user_params)
         # byebug
-        if @user.valid?
-            @token = encode_token(user_id: @user.id)
-            render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
-        else
-            render json: { error: 'failed to create user' }, status: :not_acceptable
+        if !@user.valid?
+          render json: { error: 'failed to create user' }, status: :not_acceptable
         end
+        
+        @token = encode_token(user_id: @user.id)
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
     end
 
     def update
